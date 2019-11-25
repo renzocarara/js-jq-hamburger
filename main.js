a7941a79ca123f0498c9c3d599850e0d489d9606
Renzo Carara <renzo.carara@libero.it>
2019-11-25 15:50:38 +0100

    utilizzo classe .active per rendere visibile l'hamburger-menu

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
+// // seleziono le ancore figlie dirette di header-right
+// // ovvero l'icona hamburger
+// $('.header-right>a').click(function() {
+//     // quando l'hamburger viene cliccato
+//     // mostro, con effetto fadeIn, il menu verticale a tutto schermo
+//     $('.hamburger-menu').fadeIn(2000);
+// });
+//
+// // seleziono le ancore figlie dirette di hamburger-menu
+// // ovvero l'icona "X"
+// $('.hamburger-menu>a').click(function() {
+//     // quando la "X" viene cliccata
+//     // nascondo, con effetto fadeOut, il menu verticale a tutto schermo
+//     $('.hamburger-menu').fadeOut(1000);
+// });
+
+
 // seleziono le ancore figlie dirette di header-right
 // ovvero l'icona hamburger
 $('.header-right>a').click(function() {
     // quando l'hamburger viene cliccato
-    // mostro, con effetto fadeIn, il menu verticale a tutto schermo
-    $('.hamburger-menu').fadeIn(2000);
+    // mostro il menu verticale a tutto schermo
+    // aggiungendo la classe active all'elemento menu verticale
+    $('.hamburger-menu').addClass('active');
 });
 
 // seleziono le ancore figlie dirette di hamburger-menu
 // ovvero l'icona "X"
 $('.hamburger-menu>a').click(function() {
     // quando la "X" viene cliccata
-    // nascondo, con effetto fadeOut, il menu verticale a tutto schermo
+    // nascondo il menu verticale a tutto schermo
     $('.hamburger-menu').fadeOut(1000);
 });
