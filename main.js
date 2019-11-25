65f653e5520ca951a4db4e34d5c34295e0b35609
Renzo Carara <renzo.carara@libero.it>
2019-11-25 19:08:29 +0100

    versione finale

--- main.js
+++ main.js
@@ -1,37 +1,54 @@
-// // seleziono le ancore figlie dirette di header-right
-// // ovvero l'icona hamburger
+// SOLUZIONE 1 (NON BELLISSIMA)
+// -----------------------------------------------------------------------------
+// MENU CHE APPARE E SCOMPARE CON DELLE FADEIN/FADEOUT
+// -----------------------------------------------------------------------------
+// // gestisco l'evento click sull'icona hamburger
 // $('.header-right>a').click(function() {
-//     // quando l'hamburger viene cliccato
-//     // mostro, con effetto fadeIn, il menu verticale a tutto schermo
+//     // quando l'hamburger viene cliccato, mostro, con effetto fadeIn, l'hamburger menu
 //     $('.hamburger-menu').fadeIn(2000);
 // });
 //
-// // seleziono le ancore figlie dirette di hamburger-menu
-// // ovvero l'icona "X"
+// // gestisco l'evento click sull'icona "X"
 // $('.hamburger-menu>a').click(function() {
-//     // quando la "X" viene cliccata
-//     // nascondo, con effetto fadeOut, il menu verticale a tutto schermo
+//     // quando la "X" viene cliccata, nascondo, con effetto fadeOut, l'hamburger menu
 //     $('.hamburger-menu').fadeOut(1000);
 // });
 
 
-// seleziono le ancore figlie dirette di header-right
-// ovvero l'icona hamburger
+
+// SOLUZIONE 2 (PIU' CORRETTA)
+// -----------------------------------------------------------------------------
+// MENU CHE APPARE E SCOMPARE PER L'AGGIUNTA/RIMOZIONE DELLA CLASSE .ACTIVE
+// -----------------------------------------------------------------------------
+// // gestisco l'evento click sull'icona hamburger
+// $('.header-right>a').click(function() {
+//     // quando l'hamburger viene cliccato, mostro l'hamburger menu
+//     $('.hamburger-menu').addClass('active');
+// });
+//
+// // gestisco l'evento click sull'icona "X"
+// $('.hamburger-menu>a').click(function() {
+//     // quando la "X" viene cliccata, nascondo l'hamburger menu
+//     $('.hamburger-menu').removeClass('active');
+// });
+
+
+
+//
+// SOLUZIONE 3 (VERSIONE SUGGERITA DA LUCA)
+// -----------------------------------------------------------------------------
+// MENU CHE APPARE E SCOMPARE DAL LATO SINISTRO
+// -----------------------------------------------------------------------------
+//gestisco l'evento click sull'icona hamburger
 $('.header-right>a').click(function() {
-    // quando l'hamburger viene cliccato
-    // mostro il menu verticale a tutto schermo
-    // aggiungendo la classe active all'elemento menu verticale
-    $('.hamburger-menu').removeClass('hidden');
-    $('.hamburger-menu').addClass('active');
-    console.log("click rilevato");
+    // quando l'hamburger viene cliccato mostro il menu aggiungendo la classe enter che contiene un'animazione
+    $('.hamburger-menu').addClass('active inside');
+    $('.hamburger-menu').removeClass('outside');
 });
 
-// seleziono le ancore figlie dirette di hamburger-menu
-// ovvero l'icona "X"
+// gestisco l'evento click sull'icona "X"
 $('.hamburger-menu>a').click(function() {
-    // quando la "X" viene cliccata
-    // nascondo il menu verticale a tutto schermo
-    // rimuovendo la classe .active
-    $('.hamburger-menu').removeClass('active');
-    $('.hamburger-menu').addClass('hidden');
+    // quando la "X" viene cliccata nascondo il menu aggiungendo la classe enter che contiene un'animazione
+    $('.hamburger-menu').addClass('outside');
+    $('.hamburger-menu').removeClass('inside');
 });
